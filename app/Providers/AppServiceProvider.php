7eea044aa544721fa7a5df910a93ea8832a48e3b
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2022-03-07 11:28:55 +0100

    installation de laravel/helper

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -27,6 +27,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+
         Article::observe(ArticleObserver::class);
     }
 }
