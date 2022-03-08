63b32326a9a7776c5fe588880c844110786efb96
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2022-03-08 17:30:12 +0100

    mise en place des actions du panel d'admin

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -5,7 +5,7 @@
 
     <h1 class="text-center my-5"> Articles </h1>
     <div class="d-flex justify-content-center">
-        <a class="btn btn-info my-2" href="{{route('articles.create')}}"><i class="fas fa-plus mx-2"></i> Ajouter un nouvel article</a>
+        <a class="btn btn-info my-2" href="{{route('articles.create')}}"><i class="fas fa-plus mx-2 mb-lg-2"></i> Ajouter un nouvel article</a>
     </div>
     <table class="table table-hover">
         <thead>
@@ -25,10 +25,29 @@
                 <td class="d-flex">
                    <a href="#" class="btn btn-warning mx-2">Editer</a>
 
+                    <button type="button" class="btn btn-danger mx-2" onclick="document.getElementById('modal-open-{{$article->id}}').style.display='block'">Supprimer</button>
                     <form action="{{route('articles.delete', $article->id)}}" method="POST">
                         @csrf
                         @method("DELETE")
-                        <button type="submit" class="btn btn-danger mx-2">Supprimer</button>
+                        <div class="modal" id="modal-open-{{$article->id}}">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title">La suppression d'un élément est definitif ! </h5>
+                                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close" onclick="document.getElementById('modal-open-{{$article->id}}').style.display='none'">
+                                            <span aria-hidden="true"></span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <p>etes-vous sur de vouloir supprimer cet élément ?</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" class="btn btn-primary">Oui</button>
+                                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal" onclick="document.getElementById('modal-open-{{$article->id}}').style.display='none'">Annuler</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </form>
                 </td>
             </tr>
