3527178c411b3d73c90987c2f2945881b316a191
Tatiana-pct <79706218+Tatiana-pct@users.noreply.github.com>
2022-03-09 20:03:48 +0100

    test ok

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -5,39 +5,25 @@ namespace Tests\Feature;
 use App\Models\User;
 use Faker\Factory;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class modelTest extends TestCase
+class DatabaseTest extends TestCase
 {
 
     use RefreshDatabase;
+
     public function testValidRegistration()
     {
-        $faker = Factory::create();
-        $email = $faker->unique()->email;
+
         $count = User::count();
 
         $response =$this->post('/register', [
-           'email' => $email,
+            'email' => '123@123.fr',
             'name'=>'test',
             'password'=> 'password',
             'password_confirmation'=> 'password'
         ]);
         $newCount = User::count();
         $this->assertNotEquals($count, $newCount);
-    }
-
-    public function testInvalideregistration()
-    {
-        $response = $this->post('/register',[
-            'email' => '',
-            'name'=>'test',
-            'password'=> 'password',
-            'password_confirmation'=> 'password'
-        ]);
-
-        $response->assertSessionHasErrors();
-
     }
 }
--- tests/Feature/RouteTest.php
+++ tests/Feature/RouteTest.php
@@ -2,13 +2,18 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
 
 class RouteTest extends TestCase
 {
+
+    use RefreshDatabase;
+
     public function testAccessAdminWithGuestRole()
     {
        $response = $this->get('/admin/articles');
@@ -17,7 +22,12 @@ class RouteTest extends TestCase
 
     public function testAccessAdminWithAdminRole()
     {
-        $admin = Auth::loginUsingId(1);
+        $admin = User::create([
+            'email'=> 'admin@admin.com',
+            'name'=> 'Admin',
+            'password'=>Hash::make('00000000'),
+            'role'=>User::ADMIN_ROLE
+        ]);
         $this->actingAs($admin);
         $response = $this->get('/admin/articles');
         $response->assertStatus(200);
